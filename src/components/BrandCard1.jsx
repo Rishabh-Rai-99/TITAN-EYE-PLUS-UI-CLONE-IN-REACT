226b3133169008549f8d821abd05b9dee2f16d99
Rishabh Rai <rishabh996633@gmail.com>
2025-03-16 18:26:29 +0530

    Fix missing images

--- src/components/BrandCard1.jsx
+++ src/components/BrandCard1.jsx
@@ -6,10 +6,10 @@ const BrandCard = () => {
   return (
     brands.map((brands)=>{
         return <div key={brands.id} className="card relative flex justify-center ">
-          <div style={{background:`url(${process.env.PUBLIC_URL}${brands.brand_img})`,backgroundPosition:"center"}} className='w-52 h-58  rounded-xl bg-cover'>
+          <div style={{background:`url(${import.meta.env.BASE_URL}${brands.brand_img})`,backgroundPosition:"center"}} className='w-52 h-58  rounded-xl bg-cover'>
           </div>
             <div className='bg-[#EFFCFE] rounded-xl flex justify-center items-center absolute -bottom-10 w-46 h-22'>
-            <img className='mix-blend-multiply' src={`${process.env.PUBLIC_URL}${brands.brand_logo}`} alt="" />
+            <img className='mix-blend-multiply' src={`${import.meta.env.BASE_URL}${brands.brand_logo}`} alt="" />
             </div>
         </div>
     })
--- src/components/BrandCard2.jsx
+++ src/components/BrandCard2.jsx
@@ -6,10 +6,10 @@ const BrandCard2 = () => {
   return (
     brandtwo.map((brands)=>{
         return <div key={brands.id} className="card relative flex justify-center">
-          <div style={{background:`url(${process.env.PUBLIC_URL}${brands.brand_img})`,backgroundPosition:"center"}} className='w-52 h-58 bg-contain rounded-xl bg-center'>
+          <div style={{background:`url(${import.meta.env.BASE_URL}${brands.brand_img})`,backgroundPosition:"center"}} className='w-52 h-58 bg-contain rounded-xl bg-center'>
           </div>
             <div className='bg-[#EFFCFE] rounded-xl flex justify-center items-center absolute -bottom-10 w-46 h-22 mx-auto'>
-            <img className='mix-blend-multiply' src={`${process.env.PUBLIC_URL}${brands.brand_logo}`} alt="" />
+            <img className='mix-blend-multiply' src={`${import.meta.env.BASE_URL}${brands.brand_logo}`} alt="" />
             </div>
         </div>
     })
